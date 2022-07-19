bbd7212552813a6f5971c1a90f579004acd786d5
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2022-07-19 08:19:36 +0200

    problem solution added

--- Ransom_Note.js
+++ Ransom_Note.js
@@ -10,15 +10,17 @@ var canConstruct = function (ransomNote, magazine) {
 
     let rans = ransomNote.split('');
     let maga = magazine.split('');
+    let alreadyHave = {};
 
     for (let i = 0, j = 0; i < maga.length; i++) {
         if (maga[i] === rans[j]) {
+            if (i in alreadyHave) continue;
+            alreadyHave[i] = maga[i];
             j++;
+            i = -1;
             if (j === rans.length) {
                 return true;
             }
-        } else {
-            j = 0;
         }
     }
     return false;
