f71cfb4ad07d842b0aed05b2d027f68d4c709fc7
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2022-07-20 09:10:01 +0200

    problem solution added

---
+++ Fizz_Buzz.js
@@ -0,0 +1,27 @@
+
+// leet code challenge 'Roman to Integer'
+// problem link https://leetcode.com/problems/fizz-buzz/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+    let arr = [];
+
+    for (let i = 0; i < n; i++) {
+        if ((i + 1) % 3 === 0 && (i + 1) % 5 === 0) {
+            arr[i] = 'FizzBuzz';
+        } else if ((i + 1) % 3 === 0) {
+            arr[i] = 'Fizz';
+        } else if ((i + 1) % 5 === 0) {
+            arr[i] = 'Buzz';
+        } else {
+            arr[i] = `${i + 1}`;
+        }
+
+    }
+    return arr;
+};
+
+console.log(fizzBuzz(15));
