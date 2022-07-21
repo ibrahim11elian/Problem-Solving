c35c5639374f6b23c972ad7abfdbe6e3032a2791
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2022-07-21 17:43:52 +0200

    problem solution added

---
+++ Middle_of_the_Linked_List.js
@@ -0,0 +1,24 @@
+
+// leet code challenge 'Middle of the Linked List'
+// problem link https://leetcode.com/problems/fizz-buzz/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+    let fast = head;
+    let slow = head;
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
