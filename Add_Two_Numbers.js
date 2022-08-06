71a99b5d8245c9d567f3c2fd4d8ccdde833eec20
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2022-08-06 21:56:58 +0200

    problem solution added

--- Add_Two_Numbers.js
+++ Add_Two_Numbers.js
@@ -1,4 +1,3 @@
-
 // leet code challenge 'Add Two Numbers'
 // problem link https://leetcode.com/problems/add-two-numbers/
 
@@ -15,42 +14,42 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-    return makeList(getsum(l1, l2));
+  return makeList(getsum(l1, l2));
 };
 
 const getsum = (l1, l2) => {
-    let v1 = 0;
-    let v2 = 0;
-    let carry = 0;
-    let result = [];
-    while (l1 || l2 || carry) {
-        if (l1) {
-            v1 = l1.val;
-            l1 = l1.next;
-        }
-        if (l2) {
-            v2 = l2.val;
-            l2 = l2.next;
-        }
-        let sum = v1 + v2 + carry;
-        carry = Math.floor(sum / 10);
-        let digit = sum % 10;
-        result.push(digit);
-        v1 = 0;
-        v2 = 0;
+  let v1 = 0;
+  let v2 = 0;
+  let carry = 0;
+  let result = [];
+  while (l1 || l2 || carry) {
+    if (l1) {
+      v1 = l1.val;
+      l1 = l1.next;
     }
-    return result;
+    if (l2) {
+      v2 = l2.val;
+      l2 = l2.next;
+    }
+    let sum = v1 + v2 + carry;
+    carry = Math.floor(sum / 10);
+    let digit = sum % 10;
+    result.push(digit);
+    v1 = 0;
+    v2 = 0;
+  }
+  return result;
 };
 
 const makeList = (strinList) => {
-    let head = new ListNode(0);
-    head.val = strinList[0];
-    let temp = head;
-    for (i = 1; i < strinList.length; i++) {
-        let node = new ListNode(0);
-        node.val = strinList[i];
-        temp.next = node;
-        temp = temp.next;
-    }
-    return head;
+  let head = new ListNode(0);
+  head.val = strinList[0];
+  let temp = head;
+  for (i = 1; i < strinList.length; i++) {
+    let node = new ListNode(0);
+    node.val = strinList[i];
+    temp.next = node;
+    temp = temp.next;
+  }
+  return head;
 };
