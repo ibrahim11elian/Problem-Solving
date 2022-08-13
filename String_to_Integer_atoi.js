242753f9c047089e7e60380e772247e60e2944ba
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2022-08-13 13:03:54 +0200

    problem solution added

--- String_to_Integer_atoi.js
+++ String_to_Integer_atoi.js
@@ -3,7 +3,24 @@
  * @return {number}
  */
 var myAtoi = function (s) {
-  return +s.trimStart(" ");
+  // if whitespaces then ignore.
+  s = s.trimStart(" ");
+
+  const regEx = /^((-?|\+?)\d+)/i;
+  if (s.match(regEx)) {
+    const number = Number(s.match(regEx)[0]);
+    if (valid(number)) return number;
+    const max = Math.pow(2, 31) - 1;
+    const min = -Math.pow(2, 31);
+    return number < 1 ? min : max;
+  } else {
+    return 0;
+  }
 };
 
-console.log(myAtoi(" a       233"));
+let valid = (number) => {
+  if (number > -Math.pow(2, 31) - 1 && number < Math.pow(2, 31)) return true;
+  return false;
+};
+
+console.log(myAtoi("-2147483649"));
