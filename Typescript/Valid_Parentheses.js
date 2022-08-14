87250d0f4e48db4b0c615732a6dca2f93492da32
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2022-08-14 20:29:07 +0200

    problem solution added

--- Typescript/Valid_Parentheses.js
+++ Typescript/Valid_Parentheses.js
@@ -1,10 +1,45 @@
-// function isValid(s: string): boolean {
-// //   let i: number = 0;
-// //   const open: "(" | "{" | "[";
-// //   const closed: ")" | "}" | "]";
-// //   let arr: string[];
-// //   while (s[i]) {
-// //     if(s[i]  === open){
-// //     }
-// //   }
-// }
+function isValid(s) {
+    var i = 0;
+    var open = {
+        "(": 1,
+        "{": 2,
+        "[": 3
+    };
+    var closed = {
+        ")": 1,
+        "}": 2,
+        "]": 3
+    };
+    var arr = [];
+    if (s.length < 2)
+        return false;
+    while (s[i]) {
+        if (s[i] in open) {
+            arr.push(s[i]);
+        }
+        else if (s[i] in closed) {
+            if (match(s[i]) === arr[arr.length - 1]) {
+                arr.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+function match(s) {
+    switch (s) {
+        case ")":
+            return "(";
+        case "}":
+            return "{";
+        case "]":
+            return "[";
+    }
+}
+console.log(isValid("()[]{}"));
--- Typescript/Valid_Parentheses.ts
+++ Typescript/Valid_Parentheses.ts
@@ -1,11 +1,43 @@
-// function isValid(s: string): boolean {
-// //   let i: number = 0;
-// //   const open: "(" | "{" | "[";
-// //   const closed: ")" | "}" | "]";
-// //   let arr: string[];
-// //   while (s[i]) {
-// //     if(s[i]  === open){
+function isValid(s: string): boolean {
+  let i: number = 0;
+  const open: object = {
+    "(": 1,
+    "{": 2,
+    "[": 3,
+  };
+  const closed: object = {
+    ")": 1,
+    "}": 2,
+    "]": 3,
+  };
+  let arr: string[] = [];
+  if (s.length < 2) return false;
+  while (s[i]) {
+    if (s[i] in open) {
+      arr.push(s[i]);
+    } else if (s[i] in closed) {
+      if (match(s[i] as ")" | "}" | "]") === arr[arr.length - 1]) {
+        arr.pop();
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+    i++;
+  }
+  return true;
+}
 
-// //     }
-// //   }
-// }
+function match(s: ")" | "}" | "]"): string {
+  switch (s) {
+    case ")":
+      return "(";
+    case "}":
+      return "{";
+    case "]":
+      return "[";
+  }
+}
+
+console.log(isValid("()[]{}"));
