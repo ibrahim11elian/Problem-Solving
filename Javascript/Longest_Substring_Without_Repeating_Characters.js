ea9cef91fbe98cb9f6255ddc136930083c2266ff
ibrahim11elian <ibrahim11elian@gmail.com>
2023-12-09 23:27:35 +0200

    correct variable name

--- Javascript/Longest_Substring_Without_Repeating_Characters.js
+++ Javascript/Longest_Substring_Without_Repeating_Characters.js
@@ -1,4 +1,3 @@
-
 // leet code challenge 'Longest Substring Without Repeating Characters'
 // problem link https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
@@ -7,33 +6,30 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let supSrting = s[0];
-    let max = 0;
-    let len = s.length;
+  let supString = s[0];
+  let max = 0;
+  let len = s.length;
 
-
-    if (len === 0) {
-        return 0;
-    } else if (len === 2 && s[0] !== s[1]) {
-        return len;
+  if (len === 0) {
+    return 0;
+  } else if (len === 2 && s[0] !== s[1]) {
+    return len;
+  }
+  for (let i = 1; i < len; i++) {
+    if (supString.includes(s[i])) {
+      max = max < supString.length ? supString.length : max;
+      let j = i - 1;
+      while (s[j] !== s[i]) {
+        j--;
+      }
+      i = j;
+      supString = "";
+      continue;
+    } else {
+      supString += s[i];
     }
-    for (let i = 1; i < len; i++) {
-        if (supSrting.includes(s[i])) {
-            max = max < supSrting.length ? supSrting.length : max;
-            let j = i - 1;
-            while (s[j] !== s[i]) {
-                j--;
-            }
-            i = j;
-            supSrting = '';
-            continue;
-        } else {
-            supSrting += s[i];
-        }
-
-    }
-    return max < supSrting.length ? supSrting.length : max;
-
+  }
+  return max < supString.length ? supString.length : max;
 };
 
 console.log(lengthOfLongestSubstring("pwwkew"));
