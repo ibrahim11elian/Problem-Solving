063235004ae64080a78039647c1f9641272630ac
ibrahim11elian <ibrahim11elian@gmail.com>
2023-12-12 23:53:42 +0200

    Find the Index of the First Occurrence in a String problem added

---
+++ Javascript/Find_the_Index_of_the_First_Occurrence_in_a_String.js
@@ -0,0 +1,14 @@
+// leet code challenge 'Find the Index of the First Occurrence in a String'
+// problem link https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  // Returns the position of the first occurrence of a substring.
+  return haystack.indexOf(needle) ? haystack.indexOf(needle) : -1;
+};
+
+console.log(strStr("leetcode", "leeto"));
