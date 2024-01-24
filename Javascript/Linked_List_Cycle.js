e044430c71d5391ab028070afd23efbd9a07746d
ibrahim11elian <ibrahim11elian@gmail.com>
2024-01-24 13:58:45 +0200

    Linked List Cycle problem added

---
+++ Javascript/Linked_List_Cycle.js
@@ -0,0 +1,26 @@
+// leet code challenge 'Linked List Cycle'
+// problem link https://leetcode.com/problems/linked-list-cycle/description/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let set = new Set();
+  while (head) {
+    if (set.has(head)) {
+      return true;
+    }
+    set.add(head);
+    head = head.next;
+  }
+  return false;
+};
