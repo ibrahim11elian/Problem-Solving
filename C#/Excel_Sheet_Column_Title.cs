5d3995c076ae7fcd3c47b690d05274e08b931448
ibrahim11elian <ibrahim11elian@gmail.com>
2024-04-13 23:59:09 +0200

    Excel Sheet Column Title problem added

--- C#/Excel_Sheet_Column_Title.cs
+++ C#/Excel_Sheet_Column_Title.cs
@@ -2,17 +2,31 @@
 // problem link https://leetcode.com/problems/Excel-Sheet-Column-Title/description/
 
 public class Solution {
+    // This method converts a column number into its corresponding Excel column title.
+    // For example, column number 1 corresponds to "A", 2 corresponds to "B", and so on.
     public string ConvertToTitle(int columnNumber) {
+        // Initialize an empty string to store the resulting Excel column title.
         string result = "";
+        
+        // Iterate until the columnNumber is greater than 0.
         while(columnNumber > 0)
         {
+            // Calculate the remainder when (columnNumber - 1) is divided by 26.
             int remind = (columnNumber - 1) % 26;
-            result +=Convert.ToChar(((int)'A') + remind);
+            
+            // Convert the remainder into the corresponding character and append it to the result.
+            result += Convert.ToChar(((int)'A') + remind);
+            
+            // Update columnNumber for the next iteration by dividing (columnNumber - 1) by 26.
             columnNumber = (columnNumber - 1) / 26;
         }
+        
+        // Reverse the characters in the result string since they were added in reverse order.
         char[] stringArray = result.ToCharArray();
         Array.Reverse(stringArray);
         string reversedStr = new string(stringArray);
+        
+        // Return the final Excel column title.
         return reversedStr;
     }
 }
