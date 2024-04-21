d8365a77ccd54fc75027cc6bcb0c3385f32c6a80
ibrahim11elian <ibrahim11elian@gmail.com>
2024-04-21 17:58:30 +0200

    Customers Who Never Order problem added

---
+++ SQL/Customers_Who_Never_Order.sql
@@ -0,0 +1,8 @@
+-- // leet code challenge 'Customers Who Never Order'
+-- // problem link https://leetcode.com/problems/customers-who-never-order/description/
+
+-- Write your PostgreSQL query statement below
+SELECT c.name as Customers 
+FROM customers c 
+WHERE id NOT IN 
+(SELECT DISTINCT customerId FROM orders);
